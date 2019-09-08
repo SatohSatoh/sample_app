bec7cbe6cd879dd9f056405d2200ab6fea234d32
佐藤誠也 <satohseiya@satouseiyanoMacBook-Pro.local>
2019-09-08 15:30:29 +0900

    Update application

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  resources :test
+  
   root 'test#top'
   get 'test/top' => 'test#top'
-  get 'test/new' => 'test#new'
-  get 'test/index' => 'test#index'
-  post 'test/create' => 'test#create'
-  get 'test/:id/edit' => 'test#edit'
-  post 'test/:id/update' => 'test#update'
-  post 'test/:id/destroy' => 'test#destroy'
+  get '/test/new' => 'test#new'
+  get '/test/index' => 'test#index'
+  post '/test/create' => 'test#create'
+  get '/test/:id/edit' => 'test#edit'
+  post '/test/:id/update' => 'test#update'
+  post '/test/:id/destroy' => 'test#destroy'
 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
